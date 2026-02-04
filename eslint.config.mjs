1dd967f4f4fd7b0fa2b0acdd42250ab439474ee3
Ilja Weber <ilja@endereco.de>
2026-02-04 11:51:48 +0100

    Add eslint for js files
    Until now we had to manually use eslint for javascript files
    the developers create. Which is prone to human error, since
    a developer might forget to verify a file.
    
    So I've extended the eslint config to test the endereco.js and
    files in src/Resources/app (but not the compiled files in public)
    
    And made it part of regular "composer run qa" scripts collection.
    
    Now it should be harder to leave error unnoticed.
    
    Eslint is basic though. Type errors in javascript code will be missed.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,11 +3,33 @@ import globals from "globals";
 import js from "@eslint/js";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"] },
   {
-    files: ["**/*.{js,mjs,cjs}"],
+    files: ["endereco.js"],
     languageOptions: {
-      globals: globals.browser }
+      globals: {
+        ...globals.browser,
+        jQuery: "readonly",
+        enderecoLoadAMSConfig: "readonly",
+      }
     },
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+    plugins: { js },
+    extends: ["js/recommended"],
+    rules: {
+      "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    },
+  },
+  {
+    files: ["src/Resources/app/**/*.js"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        Shopware: "readonly",
+      }
+    },
+    plugins: { js },
+    extends: ["js/recommended"],
+    rules: {
+      "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    },
+  },
 ]);
